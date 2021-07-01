5549275371a7dac399ef5320abdf2ec59a85bdca
Nickolay Kolesnikov <kolyan654d@gmail.com>
2021-07-01 19:20:32 +0300

    Added one service with package

--- lib/router.rb
+++ lib/router.rb
@@ -5,14 +5,14 @@ class Router
   self.unmarshal_class_method = :decode
   self.service_name = 'Codebreaker'
 
-  rpc :Registration, ::RegistrationPb::Request, ::RegistrationPb::Reply
+  # rpc :Registration, ::RegistrationPb::Request, ::RegistrationPb::Reply
   rpc :Guess, ::GuessPb::Request, ::GuessPb::Reply
   rpc :Hint, ::HintPb::Request, ::HintPb::Reply
   rpc :Statistic, ::StatisticPb::Request, ::HintPb::Reply
 
-  def registration(request_args, _unused_call)
-    RegistrationPb::Reply.new(message: RegistrationService.call(request_args))
-  end
+  # def registration(request_args, _unused_call)
+  #   RegistrationPb::Reply.new(message: RegistrationService.call(request_args))
+  # end
 
   def guess(request_args, _unused_call)
     GuessPb::Reply.new(answer: GuessService.call(request_args))
--- server.rb
+++ server.rb
@@ -12,6 +12,7 @@ class Server
     s = GRPC::RpcServer.new
     s.add_http2_port('0.0.0.0:50051', :this_port_is_insecure)
     s.handle(Router)
+    s.handle(Registration::Codebreaker::Service)
     binding.pry
     s.run_till_terminated_or_interrupted([1, 'int', 'SIGQUIT'])
   end
