3080077adbc5c6d696ad9ffefc3a7864cf5c54f7
Nickolay Kolesnikov <kolyan654d@gmail.com>
2021-07-01 18:14:31 +0300

    Fix rubocop

--- lib/protobufs/hint_pb.rb
+++ lib/protobufs/hint_pb.rb
@@ -1,13 +1,13 @@
 Google::Protobuf::DescriptorPool.generated_pool.build do
-  add_message "hint.Request" do
+  add_message 'hint.Request' do
   end
 
-  add_message "hint.Reply" do
+  add_message 'hint.Reply' do
     optional :hint, :string, 1
   end
 end
 
 module HintPb
-  Request = Google::Protobuf::DescriptorPool.generated_pool.lookup("hint.Request").msgclass
-  Reply = Google::Protobuf::DescriptorPool.generated_pool.lookup("hint.Reply").msgclass
+  Request = Google::Protobuf::DescriptorPool.generated_pool.lookup('hint.Request').msgclass
+  Reply = Google::Protobuf::DescriptorPool.generated_pool.lookup('hint.Reply').msgclass
 end
--- lib/protobufs/statistic_pb.rb
+++ lib/protobufs/statistic_pb.rb
@@ -1,12 +1,11 @@
-
 Google::Protobuf::DescriptorPool.generated_pool.build do
-  add_file("statistic.proto", :syntax => :proto3) do
-    add_message "Request" do
+  add_file('statistic.proto', syntax: :proto3) do
+    add_message 'Request' do
     end
-    add_message "Reply" do
-      repeated :statistic, :message, 1, "Item"
+    add_message 'Reply' do
+      repeated :statistic, :message, 1, 'Item'
     end
-    add_message "Item" do
+    add_message 'Item' do
       optional :name, :string, 1
       optional :attempts, :int32, 2
       optional :hints, :int32, 3
@@ -17,7 +16,7 @@ Google::Protobuf::DescriptorPool.generated_pool.build do
 end
 
 module StatisticPb
-  Request = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("Request").msgclass
-  Reply = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("Reply").msgclass
-  Item = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("Item").msgclass
+  Request = ::Google::Protobuf::DescriptorPool.generated_pool.lookup('Request').msgclass
+  Reply = ::Google::Protobuf::DescriptorPool.generated_pool.lookup('Reply').msgclass
+  Item = ::Google::Protobuf::DescriptorPool.generated_pool.lookup('Item').msgclass
 end
